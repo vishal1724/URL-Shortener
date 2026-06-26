7f905755701cbe9452edb62a1568540d73450754
Vishal Agarwal <83177871+vishal1724@users.noreply.github.com>
2026-06-26 18:35:44 +0530

    Delete BACKEND/src/controller directory

--- BACKEND/src/controller/user.controller.js
+++
@@ -1,8 +0,0 @@
-import wrapAsync from "../utils/tryCatchWrapper.js"
-import { getAllUserUrlsDao } from "../dao/user.dao.js"
-
-export const getAllUserUrls = wrapAsync(async (req, res) => {
-    const {_id} = req.user
-    const urls = await getAllUserUrlsDao(_id)
-    res.status(200).json({message:"success",urls})
-})
