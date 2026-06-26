be7fb883cf72691abe08dd66f01b2acb918782e1
Vishal Agarwal <83177871+vishal1724@users.noreply.github.com>
2026-06-26 18:45:08 +0530

    Delete BACKEND/src/routes/short_url.route.js

--- BACKEND/src/routes/short_url.route.js
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-import { createShortUrl } from '../controller/short_url.controller.js';
-const router = express.Router();
-
-router.post("/",createShortUrl);
-
-export default router;
