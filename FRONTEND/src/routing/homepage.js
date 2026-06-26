5fccb3959177dc8c906c0aa403f92bbd3916308c
Vishal Agarwal <83177871+vishal1724@users.noreply.github.com>
2026-06-26 18:56:03 +0530

    Delete FRONTEND/src/routing directory

--- FRONTEND/src/routing/homepage.js
+++
@@ -1,9 +0,0 @@
-import { createRoute } from "@tanstack/react-router"
-import { rootRoute } from "./routeTree"
-import HomePage from "../pages/HomePage"
-
-export const homePageRoute = createRoute({
-    getParentRoute: () => rootRoute,
-    path: '/',
-    component: HomePage,
-  })
