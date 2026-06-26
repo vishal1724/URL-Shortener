81205c184c76ad76fbee7e0df2c3ed3f3a45eeae
Vishal Agarwal <83177871+vishal1724@users.noreply.github.com>
2026-06-26 18:54:26 +0530

    Delete FRONTEND/src/api directory

--- FRONTEND/src/api/shortUrl.api.js
+++
@@ -1,6 +0,0 @@
-import axiosInstance from "../utils/axiosInstance"
-
-export const createShortUrl = async (url,slug) =>{
-    const {data} = await axiosInstance.post("/api/create",{url,slug})
-    return data.shortUrl
-}
